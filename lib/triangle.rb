e4a3f0faebf125834b84dbd80829c119a067cd00
Nicholas Levenson <nicklevenson@gmail.com>
2020-11-03 13:21:12 -0800

    commit tree 2d2efbe361995337b0445463ea11e548126c03a4

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,36 @@
 class Triangle
-  # write code here
+  attr_accessor :length1, :length2, :length3
+  def initialize(length1,length2,length3)
+    @length1 = length1
+    @length2 = length2
+    @length3 = length3
+  end
+
+  def kind
+    if @length1 + @length2 > @length3 && @length2 + @length3 > @length1 && @length1 + @length3 > @length2 && @length1 > 0 && @length2 > 0 && @length3 > 0
+      if @length1 == @length2 && @length1 == @length3 && @length2 == @length3
+        return :equilateral
+      end
+      if @length1 == @length2 || @length1 == @length3 || @length2 == @length3
+        return :isosceles
+      end
+      if @length1 != @length2 && @length1 != @length3 && @length2 != @length3 
+        return :scalene 
+      end
+    
+    else
+      begin
+        raise TriangleError
+     
+      end
+    end
+  end
+  
+  
+  class TriangleError < StandardError
+    def message 
+      puts "not a triangle."
+    end
+  end 
+
 end
